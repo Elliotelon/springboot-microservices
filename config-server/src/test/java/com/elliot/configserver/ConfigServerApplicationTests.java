2276f190b3035f654520318721200b5092473131
minkyu <elliotelon@gmail.com>
2022-12-28 21:49:32 +0900

    Create and Setup Spring Cloud Config Server

---
+++ config-server/src/test/java/com/elliot/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elliot.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
